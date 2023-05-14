fd9f129830a31264c11e0fe723850d3caa57dd88
yymin1022 <yymin1022@gmail.com>
2023-05-14 12:02:13 +0900

    Update App.js : Added Left/Right Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import styled from "styled-components";
+
 const App = () => {
-  return (
-    <>
-      <h2>Hello, World!</h2>
-    </>
-  );
+    return (
+        <>
+            <ParentComponent>
+                <LeftComponent></LeftComponent>
+                <RightComponent></RightComponent>
+            </ParentComponent>
+        </>
+    );
 }
+
+const ParentComponent = styled.div`
+    height: 100vw;
+    width: 100%;
+    
+    display: flex;
+    flex-direction: row;
+`;
+
+const LeftComponent = styled.div`
+    height: 100%;
+    width: 400px;
+`;
+
+const RightComponent = styled.div`
+    height: 100%;
+    width: 100%;
+`;
 
 export default App;
